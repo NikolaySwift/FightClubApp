5888c14f16a1ef4c3f24bbde5cb8147bcf4f3d61
NikolayD <volodar86@yandex.ru>
2024-08-20 22:33:28 +0300

    add structs Statistics, Item and Inventory

--- FightClubApp/Models/Character.swift
+++ FightClubApp/Models/Character.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+struct Statistics {
+    let totalFights: Int
+    let wins: Int
+    let losses: Int
+}
+
+struct Inventory {
+    private var equipment: [ItemType: Item?]
+    private var items: [Item]
+    
+    init(items: [Item] = []) {
+        equipment = [
+            .equipped_head: nil,
+            .equipped_body: nil,
+            .equipped_legs: nil,
+            .equipped_hands: nil
+        ]
+        self.items = items
+    }
+    
+    mutating func addItem(_ item: Item) {
+        items.append(item)
+    }
+    
+    mutating func removeItem(at index: Int) {
+        items.remove(at: index)
+    }
+}
+
+struct Item {
+    let name: String
+    let description: String
+    let type: ItemType
+    let price: Int
+    let effects: [SecondaryParameter: Int]
+}
+
+enum ItemType {
+    case used
+    case equipped_head
+    case equipped_body
+    case equipped_legs
+    case equipped_hands
+}
+
+enum SecondaryParameter {
+    case currentHealth
+    case maximumHealth
+    case currentEnergy
+    case maximumEnergy
+    case damage
+    case defence
+    case dodgeChance
+}
