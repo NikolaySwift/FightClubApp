d84e4c266b8ff4c603912caeed6590d789c49c0e
Evgeniy <ev.shipitsyn@yandex.ru>
2024-08-20 19:09:09 +0300

    add Account, ItemEffect, ShopItem into model

--- FightClubApp/Models/Character.swift
+++ FightClubApp/Models/Character.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Account {
+    let login: String
+    let password: String
+    //let character: Character
+}
+
+struct ItemEffect {
+let type: EffectType
+let value: Int
+}
+
+enum EffectType: String {
+    case strenght = "Strenght"
+    case endurance = "Endurance"
+    case health = "Health"
+    case energy = "Energy"
+}
+
+struct ShopItem {
+    //let item: Item
+    let quantity: Int
+}
